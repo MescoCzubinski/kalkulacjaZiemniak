e865e73331d89f36d5b8f52863c7084fd26a9c38
Mieszko Czubiński <mieszkoczubinski@gmail.com>
2025-02-10 22:37:21 +0100

    first commit

---
+++ src/script.js
@@ -0,0 +1,25 @@
+function format(inputString) {
+  return inputString
+    .replace(/^0/g, "")
+    .replace(/,/g, ".")
+    .replace(/^\.($|[^0-9])/, "0.")
+    .replace(/\.{2,}/g, ".")
+    .replace(/(.*?\..*?)\./g, "$1")
+    .replace(/(\d+\.\d{2})\d*/g, "$1")
+    .replace(/[a-zA-Z]+/g, "");
+}
+document.querySelector("input").addEventListener("input", (event) => {
+  const inputField = event.target;
+  inputField.value = format(inputField.value);
+  console.log(inputField.value);
+});
+const visitedElements = document.querySelectorAll("input, select");
+visitedElements.forEach((input) => {
+  input.addEventListener("blur", function () {
+    if (this.value) {
+      this.classList.add("visited");
+    } else {
+      this.classList.remove("visited");
+    }
+  });
+});
